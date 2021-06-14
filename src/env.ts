36f98b157d5a14ed68fcd6a0c66747b583e4b68c
siner308 <siner308@gmail.com>
2021-06-14 22:20:53 +0900

    서버 env 추가

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,19 @@
-interface Kakao {
-  mapApiKey: string | undefined;
-}
-
 interface Env {
-  kakao: Kakao;
+  kakao: {
+    mapApiKey: string | undefined;
+  };
+  api: {
+    host: string | undefined;
+  };
 }
 
 export const env: Env = {
   kakao: {
-    mapApiKey: process.env.REACT_APP_KAKAO_MAP_API_KEY,
+    mapApiKey: process.env.REACT_APP_KAKAO_MAP_API_KEY?.toString(),
+  },
+  api: {
+    host: process.env.REACT_APP_API_HOST?.toString(),
   },
 };
+
+console.log(env);
--- src/states/places/placeMap.ts
+++ src/states/places/placeMap.ts
@@ -1,5 +1,7 @@
-import { selector } from 'recoil';
+import { atom, selector } from 'recoil';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import { env } from '../../env';
 
 export interface IPlace {
   id: string;
@@ -14,10 +16,42 @@ export const placeMapState = selector<{ [key: string]: IPlace }>({
   key: 'places',
   get: async () => {
     const placeMap: { [key: string]: IPlace } = {};
-    const response = await axios.get<IPlace[]>('http://localhost:8000/api/places/');
+    console.log(env.api.host);
+    const response = await axios.get<IPlace[]>(env.api.host + '/api/places/');
     Object.values(response.data).forEach((place) => {
       placeMap[place.id] = place;
     });
     return placeMap;
   },
 });
+
+// const places: IPlace[] = [
+//   {
+//     id: uuidv4(),
+//     latitude: 37.529299,
+//     longitude: 127.116387,
+//     name: '신전떡볶이 풍납점',
+//     phone: '0212341234',
+//   },
+//   {
+//     id: uuidv4(),
+//     latitude: 37.532261,
+//     longitude: 127.121712,
+//     name: '셀프하우스',
+//   },
+//   {
+//     id: uuidv4(),
+//     latitude: 37.528799,
+//     longitude: 127.11747,
+//     name: '감탄떡볶이 풍납2동점',
+//     phone: '0212341234',
+//   },
+// ];
+//
+// export const placeMapState = atom<{ [key: string]: IPlace }>({
+//   key: 'places',
+//   default: places.reduce((map: { [key: string]: IPlace }, place: IPlace) => {
+//     map[place.id] = place;
+//     return map;
+//   }, {}),
+// });
