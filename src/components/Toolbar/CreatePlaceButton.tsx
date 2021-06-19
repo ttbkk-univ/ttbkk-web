24652b0742228d86b9a672f8beb0874296d48ecd
siner308 <siner308@gmail.com>
2021-06-19 17:16:20 +0900

    useCallback으로 마우스 클릭 이벤트 수정

--- src/components/Toolbar/CreatePlaceButton.tsx
+++ src/components/Toolbar/CreatePlaceButton.tsx
@@ -1,32 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { MdPlace } from 'react-icons/md';
 import { Button } from '@material-ui/core';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { createPlaceLatLngState } from '../../states/buttons/createPlaceLatLngState';
 import { createPlaceButtonClickedState } from '../../states/buttons/createPlaceButtonClickedState';
 
-const events: ((e: any) => void)[] = [];
 function CreatePlaceButton(): React.ReactElement {
   const setCreatePlaceLatLng = useSetRecoilState(createPlaceLatLngState);
   const [createPlaceButtonClicked, setCreatePlaceButtonClicked] = useRecoilState(
     createPlaceButtonClickedState,
   );
 
-  const clickEvent = (e: any): void => {
+  const clickEvent = useCallback((e: any): void => {
     setCreatePlaceLatLng({
       latitude: e.latLng.getLat(),
       longitude: e.latLng.getLng(),
     });
-  };
+  }, []);
 
   useEffect(() => {
+    if (!window.kakao || !window.map) return;
     if (createPlaceButtonClicked) {
-      events.push(clickEvent);
       window.map.setCursor('crosshair');
       window.kakao.maps.event.addListener(window.map, 'click', clickEvent);
     } else {
-      window.map?.setCursor('grab');
-      window.kakao?.maps.event.removeListener(window.map, 'click', events.pop());
+      window.map.setCursor('grab');
+      window.kakao.maps.event.removeListener(window.map, 'click', clickEvent);
     }
   }, [createPlaceButtonClicked]);
 
