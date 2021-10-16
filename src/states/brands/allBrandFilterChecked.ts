36eb5d5bb716abab046573d946b3b1ec60c69d34
siner308 <siner308@gmail.com>
2021-10-16 21:59:24 +0900

    brand filter 전역으로 롤백

--- src/states/brands/allBrandFilterChecked.ts
+++ src/states/brands/allBrandFilterChecked.ts
@@ -1,10 +1,5 @@
 import { atom } from 'recoil';
 
-export const brandFilterState = atom<{ name: string; visible: boolean }[]>({
-  key: 'brandFilter',
-  default: [],
-});
-
 export const allBrandFilterCheckedState = atom<boolean>({
   key: 'allBrandFilterChecked',
   default: true,
